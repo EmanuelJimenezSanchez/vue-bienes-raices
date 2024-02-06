87f4206fc43135bdece746bf71fafb6f6a2b1bb2
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-02-06 11:36:22 -0600

    Add authentication functionality to App.vue and auth.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { useAuthStore } from './stores/auth'
+
+const auth = useAuthStore()
+
 </script>
 
 <template>
@@ -24,13 +28,17 @@ import { RouterView } from 'vue-router'
         </template>
 
         <template v-slot:append>
-          <v-btn :to="{ name: 'home' }">
-            Inicio
-          </v-btn>
+          <div v-if="auth.isAuth">
+            <v-btn :to="{ name: 'admin-propiedades' }">Admin</v-btn>
+  
+            <v-btn @click="auth.logout">Cerrar Sesión</v-btn>
+          </div>
 
-          <v-btn :to="{ name: 'login' }">
-            Iniciar Sesión
-          </v-btn>
+          <div v-else>
+            <v-btn :to="{ name: 'home' }">Inicio</v-btn>
+  
+            <v-btn :to="{ name: 'login' }">Iniciar Sesión</v-btn>
+          </div>
         </template>
       </v-app-bar>
 
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,12 +1,15 @@
 import { ref, computed, onMounted } from 'vue'
 import { defineStore } from 'pinia'
 import { useFirebaseAuth } from 'vuefire'
-import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import { useRouter } from 'vue-router'
 
 export const useAuthStore = defineStore('auth', () => {
 
   const auth = useFirebaseAuth()
   const authUser = ref(null)
+  const router = useRouter()
+
   const errorMsg = ref('')
   const errorCodes = {
     'auth/invalid-email': 'El email es inválido',
@@ -31,20 +34,36 @@ export const useAuthStore = defineStore('auth', () => {
         errorMsg.value = ''
         authUser.value = userCredential.user
 
-        console.log(authUser.value)
+        router.push({ name: 'admin-propiedades' })
+
       })
       .catch((error) => {
         errorMsg.value = errorCodes[error.code]
       })
   }
 
+  const logout = () => {
+    signOut(auth).then(() => {
+      authUser.value = null
+      router.push({ name: 'login' })
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
+
   const hasError = computed(() => {
     return errorMsg.value
   })
 
+  const isAuth = computed(() => {
+    return authUser.value
+  })
+
   return {
     login,
+    logout,
     hasError,
-    errorMsg
+    errorMsg,
+    isAuth
   }
 })
