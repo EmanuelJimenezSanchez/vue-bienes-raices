8ab67a4be1e3348d4676de57199803d442657191
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-02-07 18:29:48 -0600

    Add propertyPrice computed property and display property details in AdminView

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,7 +1,7 @@
 <script setup>
   import usePropiedades from '@/composables/usePropiedades'
 
-  const { propiedadesCollection } = usePropiedades()
+  const { propiedadesCollection, propertyPrice } = usePropiedades()
 </script>
 
 <template>
@@ -14,5 +14,40 @@
   >
     Nueva Propiedad
   </v-btn>
+
+  <v-card class="mx-auto mt-10 elevation-0">
+    <v-list>
+      <v-list-item
+        v-for="propiedad in propiedadesCollection"
+        :key="propiedad.id"
+      >
+        <template v-slot:prepend>
+          <v-list-item-media :start="true">
+            <img width="180" :src="propiedad.imagen" />
+          </v-list-item-media>
+        </template>
+
+        <v-list-item-title>{{ propiedad.titulo }}</v-list-item-title>
+        <v-list-item-subtitle>{{ propertyPrice(propiedad.precio) }}</v-list-item-subtitle>
+
+        <template v-slot:append>
+          <v-btn
+            color="info"
+            flat
+            class="mr-3"
+          >
+            Editar
+          </v-btn>
+
+          <v-btn
+            color="red-darken-3"
+            flat
+          >
+            Eliminar
+          </v-btn>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-card>
 </template>
 
